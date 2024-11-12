312d8ef59a91b9a8cdf4810bff1ad4db603276ca
Axror <100289253+Asalboyev@users.noreply.github.com>
2024-11-12 16:37:26 +0500

    fix

--- database/migrations/2024_10_01_025613_create_products_table.php
+++ database/migrations/2024_10_01_025613_create_products_table.php
@@ -19,16 +19,16 @@ return new class extends Migration
             $table->decimal('thickness', 8, 3);  // Thickness (Stenka) with three decimal precision
             $table->float('length');  // Length in meters
             $table->string('metal_type');  // Metal Type (Qora/Oq)
-            $table->decimal('price_per_ton', 12, 2);  // Price per Ton with two decimal precision
+            $table->float('price_per_ton');  // Price per Ton with two decimal precision
             $table->float('length_per_ton');  // Length per Ton (meters)
-            $table->decimal('price_per_meter', 12, 3);  // Price per Meter with three decimal precision
-            $table->decimal('price_per_item', 12, 3);  // Price per Item
-            $table->decimal('price_per_package', 12, 2);  // Price per Package with two decimal precision
+            $table->float('price_per_meter');  // Price per Meter with three decimal precision
+            $table->float('price_per_item');  // Price per Item
+            $table->float('price_per_package');  // Price per Package with two decimal precision
             $table->float('items_per_package')->nullable();  // Items per Package
             $table->float('package_weight');  // Package Weight in Tons
             $table->float('package_length');  // Package Length (meters)
-            $table->decimal('weight_per_item', 12, 6)->nullable();  // Weight per item in tons with six decimal precision
-            $table->decimal('weight_per_meter', 12, 6);  // Weight per meter
+            $table->float('weight_per_item')->nullable();  // Weight per item in tons with six decimal precision
+            $table->float('weight_per_meter');  // Weight per meter
             $table->float('total_units');  // Total units (total items across all packages)
             $table->float('bron_package')->nullable();  // Reserved packages
             $table->float('bron_one_pc')->nullable();  // Reserved single items
