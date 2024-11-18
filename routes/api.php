d670510f41a77ccc7f09a702d52551185019bd57
Axror <100289253+Asalboyev@users.noreply.github.com>
2024-11-19 01:00:10 +0500

    fix

--- routes/api.php
+++ routes/api.php
@@ -32,10 +32,16 @@ Route::prefix('v1')->group(function () {
     Route::post('refresh', [LoginController::class, 'refresh']);
     Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
-    Route::middleware(['auth:sanctum', 'role:admin,seller'])->group(function () {
 
-//        Route::get('profile', [CustomersController::class, 'me']);
-
+    Route::middleware(['auth:sanctum', 'role:admin,seller,warehouseman,guard'])->group(function () {
+        Route::delete('customer/{id}', [CustomersController::class, 'apiDestroy'])->name('api.customer.destroy');
+        Route::delete('customer/{id}', [CustomersController::class, 'apiDestroy'])->name('api.customer.destroy');
+        Route::get('products', [ProductController::class, 'index'])->name('products.index');
+        Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
+        Route::put('products/{id}/add-package', [ProductController::class, 'addPackage'])->name('products.add-package');
+        Route::put('products/{id}/update-price', [ProductController::class, 'updatePrice'])->name('products.update-price');
+        Route::get('products/{id}/sklad', [ProductController::class, 'getSklad']);
+        Route::get('/sales/product/{id}', [\App\Http\Controllers\Backend\MainController::class, 'getSalesDetailsForProduct']);
         Route::get('customers', [CustomersController::class, 'apiIndex'])->name('api.customers.index');
         Route::get('customer/{id}', [CustomersController::class, 'apiShow'])->name('api.customer.show');
         Route::post('customers', [CustomersController::class, 'apiStore'])->name('api.customers.store');
@@ -44,26 +50,12 @@ Route::prefix('v1')->group(function () {
         Route::get('orders/{order}', [OrderController::class, 'show']);
         Route::get('orders', [OrderController::class, 'index']);
 //        Route::put('/orders/{order}', [OrderController::class, 'update']);
-
         Route::delete('/orders/{order}', [OrderController::class, 'destroy']);
-        Route::middleware(['role:admin'])->group(function () {
-            Route::delete('customer/{id}', [CustomersController::class, 'apiDestroy'])->name('api.customer.destroy');
-        });
-    });
-    Route::middleware(['auth:sanctum', 'role:admin,seller,warehouseman,guard'])->group(function () {
-        Route::middleware(['role:warehouseman,seller,admin'])->group(function () {
-            Route::delete('customer/{id}', [CustomersController::class, 'apiDestroy'])->name('api.customer.destroy');
-            Route::get('products', [ProductController::class, 'index'])->name('products.index');
-            Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
-            Route::put('products/{id}/add-package', [ProductController::class, 'addPackage'])->name('products.add-package');
-            Route::put('products/{id}/update-price', [ProductController::class, 'updatePrice'])->name('products.update-price');
-            Route::get('products/{id}/sklad', [ProductController::class, 'getSklad']);
-            Route::get('/sales/product/{id}', [\App\Http\Controllers\Backend\MainController::class, 'getSalesDetailsForProduct']);
-
-        });
-        Route::get('/statuses', [OrderController::class, 'status']);
-
-        Route::get('orders', [OrderController::class, 'index']);
+//        Route::middleware(['role:warehouseman,seller,admin'])->group(function () {
+//
+//        });
+            Route::get('/statuses', [OrderController::class, 'status']);
+            Route::get('orders', [OrderController::class, 'index']);
             Route::put('orders/{id}/update', [OrderController::class, 'update_status']);
             Route::post('products/{id}/photos', [ProductController::class, 'addPhotos']);
             Route::delete('/photos/{photoId}', [ProductController::class, 'deletePhoto']);
@@ -71,23 +63,15 @@ Route::prefix('v1')->group(function () {
             Route::get('profile', [CustomersController::class, 'me']);
             Route::get('customers', [CustomersController::class, 'apiIndex'])->name('api.customers.index');
             Route::get('customer/{id}', [CustomersController::class, 'apiShow'])->name('api.customer.show');
-
-
-        Route::middleware(['role:admin,seller'])->group(function () {
-//                Route::get('products/{id}/sklad', [ProductController::class, 'getSklad']);
-//                Route::get('products', [ProductController::class, 'index'])->name('products.index');
-//                Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
-                Route::get('/sales/product/{id}', [\App\Http\Controllers\Backend\MainController::class, 'getSalesDetailsForProduct']);
+            Route::get('/sales/product/{id}', [\App\Http\Controllers\Backend\MainController::class, 'getSalesDetailsForProduct']);
 //                Route::get('/statuses', [OrderController::class, 'status']);
-                Route::get('categories', [\App\Http\Controllers\Api\v1\CategoriesController::class, 'index']);
-                Route::get('categories/{id}', [\App\Http\Controllers\Api\v1\CategoriesController::class, 'show']);
-            });
+            Route::get('categories', [\App\Http\Controllers\Api\v1\CategoriesController::class, 'index']);
+            Route::get('categories/{id}', [\App\Http\Controllers\Api\v1\CategoriesController::class, 'show']);
+            Route::get('/notifications', [\App\Http\Controllers\Api\v1\NotificationController::class, 'getUnreadNotifications'])->middleware('auth:sanctum');
+            Route::get('/notification-new', [\App\Http\Controllers\Api\v1\NotificationController::class, 'getAllNotifications'])->middleware('auth:sanctum');
+            Route::post('/notifications/{id}/read', [\App\Http\Controllers\Api\v1\NotificationController::class, 'markAsRead'])->middleware('auth:sanctum');
 
-        });
-    Route::get('/notifications', [\App\Http\Controllers\Api\v1\NotificationController::class, 'getUnreadNotifications'])->middleware('auth:sanctum');
-    Route::get('/notification-new', [\App\Http\Controllers\Api\v1\NotificationController::class, 'getAllNotifications'])->middleware('auth:sanctum');
-    Route::post('/notifications/{id}/read', [\App\Http\Controllers\Api\v1\NotificationController::class, 'markAsRead'])->middleware('auth:sanctum');
-
+    });
 
 });
 
